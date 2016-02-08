2fd6bb55696d4dabd8e072da9b40499c60c8e205
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-02-08 10:04:37 -0800

    Remove ^:replace "dev-resources" because Figwheel needs "resources"
    There was a 404 that I could not understand but then I saw that I was replacing the :resource-paths
    option for the dev profile. Of course Figwheel was not happy.
    
    In the process, the deps has been updated, the cljs build id changed to "website" and the classic
    "fig-dev" lein command added.

--- project.clj
+++ project.clj
@@ -5,10 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.async "0.2.371" :scope "test"]
                  [org.omcljs/om "1.0.0-alpha28"]
-                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]
                  [twitter-api "0.7.8"]
                  [com.stuartsierra/component "0.3.1"]
                  [overtone/at-at "1.2.0"]
@@ -23,12 +22,10 @@
                  [environ "1.0.2"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
-
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-3"]
             [lein-environ "1.0.2"]]
 
-  :source-paths ["src/clj" "src/cljs" "test/clj" "test/cljs"]
+  :source-paths ["src/clj" "test/clj"]
   :min-lein-version "2.0.0"
   :uberjar-name "lambdax-web-standalone.jar"
   :jvm-opts ^:replace ["-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1" "-Xverify:none"]
@@ -47,18 +44,18 @@
                                 [org.clojure/tools.namespace "0.2.11"]
                                 [org.clojure/tools.trace "0.7.9"]
                                 ;; [pjstadig/humane-test-output "0.7.1"]
-                                [mvxcvi/puget "1.0.0"]]
+                                [mvxcvi/puget "1.0.0"]
+                                [figwheel-sidecar "0.5.0-6"]]
+                 :plugins [[lein-figwheel "0.5.0-6" :exclusions [cider/cider-nrepl]]]
                  :source-paths ["src/dev" "env/dev/clj"]
-                 :resource-paths ^:replace ["dev-resources"]
                  :repl-options {:init-ns lambdax-web.dev}}
    :profiles/dev {}}
-
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :cljsbuild
   {:builds
-   [{:id "dev"
+   [{:id "website"
      :source-paths ["src/cljs"]
 
      :figwheel {:on-jsload "lambdax-web.core/on-js-reload"}
@@ -108,4 +105,6 @@
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
              }
-  :aliases {"bg-repl" ["trampoline" "repl" ":headless" ">" "repl.out " "2>" "repl.err" "<" "/dev/null" "&"]})
+  :aliases {"bg-repl" ["trampoline" "repl" ":headless" ">" "repl.out " "2>" "repl.err" "<" "/dev/null" "&"]
+            "fig-dev" ^{:doc "Start figwheel with dev profile."} ["figwheel" "website"]
+            "fig-dev*" ^{:doc "Clean and start figwheel with dev profile"} ["do" "clean" ["figwheel" "website"]]})
