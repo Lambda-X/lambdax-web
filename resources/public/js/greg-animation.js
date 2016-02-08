2d900c101784fa7675ad673969b57b3072c89896
Katarzyna Jastrzębska <kasia@scalac.io>
2016-02-08 22:44:23 +0100

    working on animation

--- resources/public/js/greg-animation.js
+++ resources/public/js/greg-animation.js
@@ -1,4 +1,4 @@
-var top, svgObj, hiddentext;
+var top, svgObj, hiddentext,comein,ltext;
 window.onload = function() {
     svgObj = document.getElementById("greg-svg");
 
@@ -13,16 +13,31 @@ window.onload = function() {
             opacity: 0
         });
 
+    comein = s.select("#comein")
+        .attr({
+            opacity: 0
+        });
 
+    ltext = s.select("lambda-text").attr({
+        display: none
+    });
+
+    //comein.animate({ transform: 't100,300' }, 1000, mina.bounce );
 }
 window.addEventListener("scroll",function(e) {
     top = window.pageYOffset;
     if(top.pageYOffset > 0) {
         svgObj.style.width = "100%";
         hiddentext.attr({opacity: 1});
+        comein.attr({opacity:1});
+        comein.animate({ transform: 't50,-500' }, 1000, mina.bounce );
+
     } else if(top.pageYOffset == 0) {
         svgObj.style.width = "140%";
         hiddentext.attr({opacity: 0});
+        comein.animate({ transform: 't50,500' }, 1000, mina.bounce );
+        comein.attr({opacity:0});
+
     }
 
 
