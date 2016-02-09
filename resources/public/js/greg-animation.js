a9f7415de162d5384938f4d139f09bf6e07dbd43
Katarzyna Jastrzębska <kasia@scalac.io>
2016-02-09 12:35:22 +0100

    first animation

--- resources/public/js/greg-animation.js
+++ resources/public/js/greg-animation.js
@@ -18,9 +18,6 @@ window.onload = function() {
             opacity: 0
         });
 
-    ltext = s.select("lambda-text").attr({
-        display: none
-    });
 
     //comein.animate({ transform: 't100,300' }, 1000, mina.bounce );
 }
@@ -28,14 +25,10 @@ window.addEventListener("scroll",function(e) {
     top = window.pageYOffset;
     if(top.pageYOffset > 0) {
         svgObj.style.width = "100%";
-        hiddentext.attr({opacity: 1});
         comein.attr({opacity:1});
-        comein.animate({ transform: 't50,-500' }, 1000, mina.bounce );
 
     } else if(top.pageYOffset == 0) {
         svgObj.style.width = "140%";
-        hiddentext.attr({opacity: 0});
-        comein.animate({ transform: 't50,500' }, 1000, mina.bounce );
         comein.attr({opacity:0});
 
     }
