9662b6849cffa58d68fcf7e7684df754cd60e5a9
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-02-17 13:37:08 -0800

    Read version from version.properties

--- src/clj/lambdax_web/system.clj
+++ src/clj/lambdax_web/system.clj
@@ -8,12 +8,32 @@
             [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
             [clojure.edn :as edn]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import java.util.Properties))
+
+(defn fetch-version!
+  "Collects the version from version.properties, returns the config with
+  added :version or passes it through if some error happened."
+  [config]
+  ;; AR - A bit messy, cause it's Java!
+  (if-let [version (-> (doto (Properties.)
+                         ;; side effect
+                         (.load (try
+                                  (some->> (if (= :prod (:build config))
+                                             (io/resource "version.properties")
+                                             (io/file "version.properties"))
+                                           (io/input-stream))
+                                  (catch java.io.FileNotFoundException ex
+                                    (println "Handled exception -" (.getMessage ex))))))
+                       (.getProperty "VERSION"))]
+    (assoc config :version version)
+    config))
 
 (defn make-config
   "Creates a default configuration map"
   []
   (-> config/defaults
+      (fetch-version!)
       (merge (into {} [(some->> (:lambdax-web-version env) (hash-map :version))
                        (some->> (:lambdax-web-port env) (Integer/parseInt) (hash-map :port))
                        (some->> (:lambdax-web-nrepl-port env) (Integer/parseInt) (hash-map :nrepl-port))
@@ -25,17 +45,16 @@
    :app (app/new-app (:name config-map)
                      (:version config-map))
    :scheduler (component/using
-               (scheduler/new-scheduler scheduler/fetch-events!
-                                        (:fetch-interval config-map))
-               [:app])
+                (scheduler/new-scheduler scheduler/fetch-events!
+                                         (:fetch-interval config-map))
+                [:app])
    :webserver (component/using
-               (webserver/new-server (:port config-map)
-                                     (:build config-map)
-                                     (or (env :access-domain)
-                                         (:access-domain config-map))
-                                     (:pre-middleware config-map)
-                                     (:post-middleware config-map))
-               [:app])
+                (webserver/new-server (:port config-map)
+                                      (:build config-map)
+                                      (or (env :access-domain)
+                                          (:access-domain config-map))
+                                      (:pre-middleware config-map)
+                                      (:post-middleware config-map))
+                [:app])
    :repl (repl/new-repl-server (:nrepl-port config-map)
                                (:build config-map))))
-
